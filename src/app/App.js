0f8e82d7fc49cafd46df59a2eb557a516b7825f6
SetRedEyes <denidraven@gmail.com>
2022-01-28 13:31:07 +0100

    hooks deleted

--- src/app/App.js
+++ src/app/App.js
@@ -6,37 +6,34 @@ import NavBar from "./components/common/navBar"
 import { publicRoutes } from "./routes"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-import AuthProvider from "./hooks/useAuth"
 import ProtectedRoute from "./components/common/protectedRoute"
 import AppLoader from "./components/ui/hoc/appLoader"
 const App = () => {
     return (
         <div>
             <AppLoader>
-                <AuthProvider>
-                    <NavBar />
+                <NavBar />
 
-                    <Switch>
-                        {publicRoutes.map(({ path, component, isProtectedRoute }) =>
-                            isProtectedRoute ? (
-                                <ProtectedRoute
-                                    key={path}
-                                    path={path}
-                                    component={component}
-                                    exact
-                                />
-                            ) : (
-                                <Route
-                                    key={path}
-                                    path={path}
-                                    component={component}
-                                    exact
-                                />
-                            )
-                        )}
-                        <Redirect to={"/gigi-janssen-store"} />
-                    </Switch>
-                </AuthProvider>
+                <Switch>
+                    {publicRoutes.map(({ path, component, isProtectedRoute }) =>
+                        isProtectedRoute ? (
+                            <ProtectedRoute
+                                key={path}
+                                path={path}
+                                component={component}
+                                exact
+                            />
+                        ) : (
+                            <Route
+                                key={path}
+                                path={path}
+                                component={component}
+                                exact
+                            />
+                        )
+                    )}
+                    <Redirect to={"/gigi-janssen-store"} />
+                </Switch>
             </AppLoader>
             <ToastContainer />
         </div>

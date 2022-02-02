e2058ce1864583d3eed8f2f5bf2aeecf17d47384
SetRedEyes <denidraven@gmail.com>
2022-02-02 16:16:10 +0100

    added midleware, packages

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const chalk = require('chalk')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const PORT = config.get('port') ?? 8080
+
+async function start() {
+  try {
+    await  mongoose.connect('mongoUri')
+    app.listen(PORT, () => {
+      console.log(
+        chalk.green(
+          `Server has been strted on port ${PORT}`
+        )
+      )
+    })
+  } catch (e) {
+    console.log(chalk.red(e.message))
+    process.exit(1)
+
+  }
+}
+
+start()

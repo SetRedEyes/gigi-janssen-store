b45ae9abef207adc57ab39fd0a31cb4f3cffa1a8
SetRedEyes <denidraven@gmail.com>
2022-02-04 18:50:27 +0100

    formated

--- server/app.js
+++ server/app.js
@@ -21,13 +21,13 @@ async function start() {
     })
     await mongoose.connect(config.get('mongoUri'))
     console.log(chalk.green('MongoDB connected.'))
-    app.listen(PORT, () => {
+    app.listen(PORT, () =>
       console.log(
         chalk.green(
-          `Server has been strted on port ${PORT}`
+          `Server has been started on port ${PORT}`
         )
       )
-    })
+    )
   } catch (e) {
     console.log(chalk.red(e.message))
     process.exit(1)

afb6ef2427052f24cda97c905439ae24d1b6699d
SetRedEyes <denidraven@gmail.com>
2022-02-04 17:36:44 +0100

    routes

--- server/app.js
+++ server/app.js
@@ -3,17 +3,20 @@ const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
 const initDatabase = require('./startUp/initDatabase')
+const routes = require('./routes')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use('/api', routes)
+
 const PORT = config.get('port') ?? 8080
 
 async function start() {
   try {
-    mongoose.connection.once('open',()=>{
+    mongoose.connection.once('open', () => {
       initDatabase()
     })
     await mongoose.connect(config.get('mongoUri'))

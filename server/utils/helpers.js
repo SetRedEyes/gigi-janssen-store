6523f7b2cb32893e86a2a5ccc927170b0cd04712
SetRedEyes <denidraven@gmail.com>
2022-03-09 12:46:18 +0100

    isAdmin

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -2,12 +2,9 @@ function generateUserData() {
   return {
     phone: '',
     city: '',
-    postOfficeNumber: '',
-    isAdmin: false
+    postOfficeNumber: ''
   }
 }
-
-
 
 module.exports = {
   generateUserData
